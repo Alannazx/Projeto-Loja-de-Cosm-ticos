a11c93c3091d806dfe98f2dc390176778f334fea
Alannazx <alannaescola3d@gmail.com>
2026-03-20 11:26:27 -0300

    códigos php
    arquivos php projeto

---
+++ check.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'config/db.php';
+
+try{
+$db = $conexao;
+
+$query = $db->query("SHOW TABLES");
+$tabelasEncontrada = $query->fetchALL(PDO::FETCH_COLUMN);
+
+$total = count($tabelasEncontradas);
+$meta = 14;
+
+echo "<h1>   Verificação do Sistema</h1>";
+
+if ($total == $meta) {
+    echo "<p style='color: green; '>Sucesso! O banco de dados está completo com as 14 tabelas.</p>";
+} else {
+$faltando = $meta = $total;
+  echo "<p style='color: red; '>Atenção! O banco possui $total tabelas. Estão <strong>faltando $faltando</strong> para chegar às 14.</p>";
+}
+echo "<h3>Tabelas encontradas no banco:</h3>";
+echo "<ul>";
+foreach ($tabelasEncontradas as $tabela) {
+    echo "<li>$tabela</11>";
+}
+echo "</ul>";
+
+} catch (Exception $e) {
+    echo "Erro ao carregar o teste: " . $e->getMessage();
+}
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+require_once __DIR__ . '/config/db.php';
+Database::getConnection();
+// Roteamento simples via GET
+$controller = $_GET['controller'] ?? 'auth';
+$action = $_GET['action'] ?? 'form';
+
+
+// Carregar controller
+switch ($controller) {
+case 'auth':
+require_once __DIR__ . '/controllers/AuthController.php';
+$c = new AuthController();
+break;
+//CRUD
+case 'produto':
+require_once __DIR__ . '/controllers/ProdutoController.php';
+$c = new ProdutoController();
+break;
+case 'entrada':
+require_once __DIR__ . '/controllers/EntradaController.php';
+$c = new EntradaController();
+break;
+case 'venda':
+require_once __DIR__ . '/controllers/VendaController.php';
+$c = new VendaController();
+break;
+case 'relatorio':
+
+
+require_once __DIR__ . '/controllers/RelatorioController.php';
+$c = new RelatorioController();
+break;
+default:
+die("Controller inválido.");
+}
+// Executar ação
+if (!method_exists($c, $action)) {
+die("Ação inválida.");
+}
+$c->$action();
